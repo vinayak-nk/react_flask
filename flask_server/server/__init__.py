1eca0afb35fca2fad3a538872a8ad2d662c5780b
Vinayak K <vinayakkurugovinakoppa@qnap.com>
2022-03-14 21:10:19 +0530

    flask_wtf, validators, validate_on_submit, GET, POST, todo form

--- flask_server/server/__init__.py
+++ flask_server/server/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+from server.forms import Todo
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SOME_KEY'
 
-@app.route('/xyz', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_wolrd():
   
   if request.method == 'POST':
@@ -19,6 +21,15 @@ def hello_wolrd():
 @app.route('/name/<string:fname>')
 def name(fname):
   return fname
+
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+  todo_form = Todo()
+  if todo_form.validate_on_submit():
+    print('======', todo_form.content.data)
+    return redirect('/')
+  return render_template('todo.html', form_some_name=todo_form)
 
 @app.route('/members')
 def members():
