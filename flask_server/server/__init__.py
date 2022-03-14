9898118f4d131a1ddf959fde6727bfe86fea2a3e
Vinayak K <vinayakkurugovinakoppa@qnap.com>
2022-03-14 22:37:50 +0530

    SQLalcemy and DB usage

--- flask_server/server/__init__.py
+++ flask_server/server/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from server.forms import Todo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SOME_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
+# db.create_all()
+
+from server.models import TodoModel
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_wolrd():
-  
+  todo_table_data = TodoModel.query.all()
   if request.method == 'POST':
     print('---------------')
     print(request.form)
@@ -15,7 +21,7 @@ def hello_wolrd():
     lname = request.form['lname']
     return redirect(url_for('name', fname=fname))
   
-  return render_template('hello.html', names_list = ['x', 'y', 'z'], request_method=request.method)
+  return render_template('hello.html', names_list = ['x', 'y', 'z'], request_method=request.method, todo_table_data=todo_table_data)
 
 
 @app.route('/name/<string:fname>')
@@ -28,6 +34,9 @@ def todo():
   todo_form = Todo()
   if todo_form.validate_on_submit():
     print('======', todo_form.content.data)
+    instance = TodoModel(content=todo_form.content.data)
+    db.session.add(instance)
+    db.session.commit() 
     return redirect('/')
   return render_template('todo.html', form_some_name=todo_form)
 
---
+++ flask_server/server/models.py
@@ -0,0 +1,8 @@
+from server import db
+
+class TodoModel(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(240))
+  
+  def __repr__(self) -> str:
+      return f'{self.content}, {self.id}'
